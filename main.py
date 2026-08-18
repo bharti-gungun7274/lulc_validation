5943e47c6c03e6589949709c92cdc756153c2faa
Ashutosh kumar tiwari <kumarashu247@gmail.com>
2026-08-18 14:03:42 +0530

    the main file updated

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
-from gui.main_window import run_app
+import sys
+
+from PySide6.QtWidgets import QApplication
+
+from gui.main_window import MainWindow
+from validation.validator import validate_csv
+
+
+def main():
+
+    # Create Qt application
+    app = QApplication(sys.argv)
+
+    # Create main window
+    window = MainWindow()
+
+    # Connect validation engine to GUI
+    window.validate_csv = validate_csv
+
+    # Show GUI
+    window.show()
+
+    # Start application
+    sys.exit(app.exec())
 
 
 if __name__ == "__main__":
-    run_app()
+    main()
